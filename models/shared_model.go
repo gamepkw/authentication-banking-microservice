fa8437698618e9c923474f36785c42ac8bd88fe0
gamepkw <game9074@gmail.com>
2023-09-02 20:15:38 +0700

    commit

---
+++ models/shared_model.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"context"
+	"time"
+)
+
+type AuthenticationService interface {
+	GenerateOtp(c context.Context, tel string) (string, error)
+	SendOtp(c context.Context, tel string) error
+	ValidateOtp(c context.Context, userOTP string, secretKey string) bool
+}
+
+type AuthenticationRepository interface {
+	SaveOtpSecret(ctx context.Context, uuid string, secretKey string) (err error)
+	GetOtpSecret(ctx context.Context, uuid string) (secretKey string, expiredAt time.Time, err error)
+}
